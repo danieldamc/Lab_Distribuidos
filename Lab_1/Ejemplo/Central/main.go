a0ddbc23b57bb93de90106388ecf111bc88d9c24
tomasnieto <tomas.nieto@sansano.usm.cl>
2022-09-10 18:35:50 -0400

    avance main local

--- Lab_1/Ejemplo/Central/main.go
+++ Lab_1/Ejemplo/Central/main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strconv"
+	"sync"
 	"time"
 
 	pb "github.com/Kendovvul/Ejemplo/Proto"
@@ -12,11 +14,66 @@ import (
 )
 
 var CASO_RESUELTO = "SI, ESTALLIDO CONTROLADO"
+var qName string
+var hostQ string
+var hostS string
+
+var ESCUADRONES_DISPONIBLES int
+
+var port_lab1 string
+var port_lab2 string
+var port_lab3 string
+var port_lab4 string
+
+var m sync.Mutex
+
+func resolver_estallido(port string, delivery amqp.Delivery) {
+	m.Lock()
+	ESCUADRONES_DISPONIBLES -= 1
+	m.Unlock()
+	connS, err := grpc.Dial(hostS+port, grpc.WithInsecure()) //crea la conexion sincrona con el laboratorio
+
+	if err != nil {
+		panic("No se pudo conectar con el servidor" + err.Error())
+	}
+
+	serviceCliente := pb.NewMessageServiceClient(connS)
+
+	for {
+		//envia el mensaje al laboratorio
+		time.Sleep(5 * time.Second) //espera de 5 segundos
+		res, err := serviceCliente.Intercambio(context.Background(),
+			&pb.Message{
+				Body: "Equipo listo?",
+			})
+
+		//fmt.Printf(connS.GetState().String() + "\n")
+		if err != nil {
+			panic("No se puede crear el mensaje " + err.Error())
+		}
+
+		defer connS.Close() //defer cierra connS al final del for
+
+		fmt.Println(string(delivery.Body) + " ha enviado: " + res.Body) //respuesta del laboratorio
+		if res.Body == CASO_RESUELTO {
+			fmt.Printf("Escuadron Retornando...\n")
+			connS.Close()
+			m.Lock()
+			ESCUADRONES_DISPONIBLES += 1
+			m.Unlock()
+			//fmt.Printf(connS.GetState().String() + "\n")
+			break
+		}
+
+	}
+}
 
 func main() {
-	qName := "Emergencias"                                           //Nombre de la cola
-	hostQ := "localhost"                                             //Host de RabbitMQ 172.17.0.1
-	hostS := "localhost"                                             //Host de un Laboratorio
+	ESCUADRONES_DISPONIBLES = 2
+	var port string
+	qName = "Emergencias"                                            //Nombre de la cola
+	hostQ = "localhost"                                              //Host de RabbitMQ 172.17.0.1
+	hostS = "localhost"                                              //Host de un Laboratorio
 	connQ, err := amqp.Dial("amqp://guest:guest@" + hostQ + ":5672") //Conexion con RabbitMQ
 
 	if err != nil {
@@ -37,6 +94,11 @@ func main() {
 
 	fmt.Println(q)
 
+	port_lab1 = ":50051" //puerto de la conexion con el laboratorio Pripyat
+	port_lab2 = ":50052" //puerto de la conexion con el laboratorio Kampala
+	port_lab3 = ":50053" //puerto de la conexion con el laboratorio Pohang
+	port_lab4 = ":50054" //puerto de la conexion con el laboratorio Renca
+
 	for {
 		fmt.Println("Esperando Emergencias")
 		chDelivery, err := ch.Consume(qName, "", true, false, false, false, nil) //obtiene la cola de RabbitMQ
@@ -44,67 +106,87 @@ func main() {
 			log.Fatal(err)
 		}
 		for delivery := range chDelivery {
-			var port string
-			port_lab1 := ":50051" //puerto de la conexion con el laboratorio Pripyat
-			port_lab2 := ":50052" //puerto de la conexion con el laboratorio Kampala
-			port_lab3 := ":50053" //puerto de la conexion con el laboratorio Pohang
-			port_lab4 := ":50054" //puerto de la conexion con el laboratorio Renca
+
+			for {
+				m.Lock()
+				if ESCUADRONES_DISPONIBLES < 1 {
+					m.Unlock()
+					fmt.Printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
+					fmt.Printf("Ningun Escuadron disponible,\nDebemos esperar que retorne uno\n")
+					fmt.Printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
+					time.Sleep(time.Second * 5)
+				} else {
+					m.Unlock()
+					break
+				}
+			}
+
+			m.Lock()
+			if ESCUADRONES_DISPONIBLES > 2 { //arreglar xd
+				ESCUADRONES_DISPONIBLES = 2
+			}
+
 			fmt.Printf("--------------------------------\n")
+			fmt.Println("numero de escuadrones disponibles: " + strconv.Itoa(ESCUADRONES_DISPONIBLES))
 			fmt.Println("Pedido de ayuda de " + string(delivery.Body)) //obtiene el primer mensaje de la cola
 			//fmt.Println(q)
+			m.Unlock()
 
 			if string(delivery.Body) == "Laboratorio Pripyat" {
 				//fmt.Printf("pripyat momento\n")
 				port = port_lab1
+				go resolver_estallido(port, delivery)
 			}
 			if string(delivery.Body) == "Laboratorio Kampala" {
 				//fmt.Printf("kampala momento\n")
 				port = port_lab2
+				go resolver_estallido(port, delivery)
 			}
 			if string(delivery.Body) == "Laboratorio Pohang" {
 				//fmt.Printf("pohang momento\n")
 				port = port_lab3
+				go resolver_estallido(port, delivery)
 			}
 			if string(delivery.Body) == "Laboratorio Renca" {
 				//fmt.Printf("renca momento\n")
 				port = port_lab4
+				go resolver_estallido(port, delivery)
 			}
 
 			//fmt.Println(port)
-			connS, err := grpc.Dial(hostS+port, grpc.WithInsecure()) //crea la conexion sincrona con el laboratorio
 
-			if err != nil {
-				panic("No se pudo conectar con el servidor" + err.Error())
-			}
-
-			serviceCliente := pb.NewMessageServiceClient(connS)
-
-			for {
-				//envia el mensaje al laboratorio
-				res, err := serviceCliente.Intercambio(context.Background(),
-					&pb.Message{
-						Body: "Equipo listo?",
-					})
-
-				//fmt.Printf(connS.GetState().String() + "\n")
-				if err != nil {
-					panic("No se puede crear el mensaje " + err.Error())
-				}
-
-				defer connS.Close() //defer cierra connS al final del for
-
-				fmt.Println(string(delivery.Body) + " ha enviado: " + res.Body) //respuesta del laboratorio
-				if res.Body == CASO_RESUELTO {
-					fmt.Printf("Escuadron Retornando...\n")
-					connS.Close()
-					//fmt.Printf(connS.GetState().String() + "\n")
-					break
-				}
-				time.Sleep(5 * time.Second) //espera de 5 segundos
-			}
 			/*
-				if len(chDelivery) == 0 {
-					break
+				connS, err := grpc.Dial(hostS+port, grpc.WithInsecure()) //crea la conexion sincrona con el laboratorio
+
+				if err != nil {
+					panic("No se pudo conectar con el servidor" + err.Error())
+				}
+
+				serviceCliente := pb.NewMessageServiceClient(connS)
+
+				for {
+					//envia el mensaje al laboratorio
+					res, err := serviceCliente.Intercambio(context.Background(),
+						&pb.Message{
+							Body: "Equipo listo?",
+						})
+
+					//fmt.Printf(connS.GetState().String() + "\n")
+					if err != nil {
+						panic("No se puede crear el mensaje " + err.Error())
+					}
+
+					defer connS.Close() //defer cierra connS al final del for
+
+					fmt.Println(string(delivery.Body) + " ha enviado: " + res.Body) //respuesta del laboratorio
+					if res.Body == CASO_RESUELTO {
+						fmt.Printf("Escuadron Retornando...\n")
+						connS.Close()
+						//fmt.Printf(connS.GetState().String() + "\n")
+						break
+					}
+					time.Sleep(5 * time.Second) //espera de 5 segundos
+
 				}*/
 		}
 	}
