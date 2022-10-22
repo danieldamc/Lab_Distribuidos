68acb2937bd51a5d03752148c261672e3a66d7c1
tomasnieto <tomas.nieto@sansano.usm.cl>
2022-10-22 18:25:41 -0300

    .

--- Lab_2/NameNode.go
+++ Lab_2/NameNode.go
@@ -1,9 +1,35 @@
 package main
 
-func upload_content(port string) {
+import (
+	"context"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/danieldamc/Lab_Distribuidos/Lab_2/Proto"
+)
+
+var Grunt_port string
+var Synth_port string
+var Cremator_port string
+
+var hostQ string
+
+func upload_content(DataNode_Port string, hostS string) {
+	connS, err := grpc.Dial(hostS+DataNode_Port, grpc.WithInsecure()) //crea la conexion sincrona con el DataNode
+
+	if err != nil {
+		panic("No se pudo conectar con el servidor" + err.Error())
+	}
+
+	service := pb.NewMessageServiceClient(connS)
+
+	res, err := service.Upload(context.Background(),
+		&pb.Message{
+			Body: "Equipo listo?",
+		})
 
 }
 
 func main() {
-
+	hostQ = "localhost"
 }
